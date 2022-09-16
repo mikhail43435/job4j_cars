0ff380fa1405055fd83a50d4e11de73a3d90ed63
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2022-09-16 17:05:33 +0300

    Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/001_ddl_init_schema.sql
@@ -0,0 +1,14 @@
+-- creating table for users
+CREATE TABLE IF NOT EXISTS auto_user
+(
+    id          SERIAL PRIMARY KEY,
+    login       TEXT NOT NULL,
+    password    TEXT NOT NULL
+);
+-- creating table for posts
+CREATE TABLE IF NOT EXISTS auto_post(
+    id              SERIAL PRIMARY KEY,
+    text            TEXT,
+    created         TIMESTAMP NOT NULL,
+    auto_user_id    INTEGER REFERENCES auto_user(id) NOT NULL
+);
